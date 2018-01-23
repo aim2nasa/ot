3ab611cd971f60283cd1e22ddb5ec1d21a601544
Rossi Kwak <rossi.kwak@gmail.com>
2018-01-23 13:27:37 +0900

    link with tee library

--- host/hello/main.c
+++ host/hello/main.c
@@ -1,11 +1,17 @@
+#include <err.h>
 #include <stdio.h>
-
 #include <tee_client_api.h>
 
 int main(int argc, char *argv[])
 {
 	TEEC_Result res;
+	TEEC_Context ctx;
 
+	res = TEEC_InitializeContext(NULL,&ctx);
+	if(res!=TEEC_SUCCESS)
+		errx(1,"TEEC_InitializeContext failed with code 0x%x",res);
+
+	TEEC_FinalizeContext(&ctx);
 	printf("Hello end\n");
 	return 0;
 }

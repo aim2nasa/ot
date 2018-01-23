de4242bd992d437f00edbf7053224ea853308c21
Rossi Kwak <rossi.kwak@gmail.com>
2018-01-23 15:17:15 +0900

    open a session with TA

--- hello/host/main.c
+++ hello/host/main.c
@@ -1,15 +1,23 @@
 #include <err.h>
 #include <stdio.h>
 #include <tee_client_api.h>
+#include <hello_ta.h>
 
 int main(int argc, char *argv[])
 {
 	TEEC_Result res;
 	TEEC_Context ctx;
+	TEEC_Session sess;
+	TEEC_UUID uuid = TA_HELLO_UUID;
+	uint32_t err_origin;
 
 	res = TEEC_InitializeContext(NULL,&ctx);
 	if(res!=TEEC_SUCCESS)
 		errx(1,"TEEC_InitializeContext failed with code 0x%x",res);
+
+	res = TEEC_OpenSession(&ctx,&sess,&uuid,TEEC_LOGIN_PUBLIC,NULL,NULL,&err_origin);
+	if(res!=TEEC_SUCCESS)
+		errx(1,"TEEC_OpenSession failed with code 0x%x origin 0x%x",res,err_origin);
 
 	TEEC_FinalizeContext(&ctx);
 	printf("Hello end\n");

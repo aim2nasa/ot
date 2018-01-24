098c276ef3a3a795816c820491f51590958d2b19
Rossi Kwak <rossi.kwak@gmail.com>
2018-01-24 12:46:00 +0900

    add debug log message for TA

--- hello/ta/hello_ta.c
+++ hello/ta/hello_ta.c
@@ -2,17 +2,21 @@
 
 TEE_Result TA_CreateEntryPoint(void)
 {
+	DMSG("has been called");
 	return TEE_SUCCESS;
 }
 
 void TA_DestroyEntryPoint(void)
 {
+	DMSG("has been called");
 }
 
 TEE_Result TA_OpenSessionEntryPoint(uint32_t param_types,
 	TEE_Param __maybe_unused params[4],
 	void __maybe_unused **sess_ctx)
 {
+	DMSG("has been called");
+
 	(void)&param_types;
 
 	return TEE_SUCCESS;
@@ -20,6 +24,7 @@ TEE_Result TA_OpenSessionEntryPoint(uint32_t param_types,
 
 void TA_CloseSessionEntryPoint(void __maybe_unused *sess_ctx)
 {
+	DMSG("has been called");
 }
 
 TEE_Result TA_InvokeCommandEntryPoint(void __maybe_unused *sess_ctx,
@@ -27,6 +32,8 @@ TEE_Result TA_InvokeCommandEntryPoint(void __maybe_unused *sess_ctx,
 	uint32_t param_types,
 	TEE_Param params[4])
 {
+	DMSG("has been called");
+
 	(void)&cmd_id;
 	(void)&param_types;
 	(void)&params;

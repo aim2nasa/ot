6d388fbdc4062631bc47b6d2f027ec8d3ae33642
Rossi Kwak <rossi.kwak@gmail.com>
2018-01-24 12:54:15 +0900

    add logs for HA

--- hello/host/main.c
+++ hello/host/main.c
@@ -11,17 +11,22 @@ int main(int argc, char *argv[])
 	TEEC_UUID uuid = TA_HELLO_UUID;
 	uint32_t err_origin;
 
+	printf("TEEC_InitializeContext...\n");
 	res = TEEC_InitializeContext(NULL,&ctx);
 	if(res!=TEEC_SUCCESS)
 		errx(1,"TEEC_InitializeContext failed with code 0x%x",res);
 	printf("TEEC_InitializeContext ok\n");
 
+	printf("TEEC_OpenSession...\n");
 	res = TEEC_OpenSession(&ctx,&sess,&uuid,TEEC_LOGIN_PUBLIC,NULL,NULL,&err_origin);
 	if(res!=TEEC_SUCCESS)
 		errx(1,"TEEC_OpenSession failed with code 0x%x origin 0x%x",res,err_origin);
 	printf("TEEC_OpenSession ok\n");
 
+	printf("TEEC_FinalizeContext...\n");
 	TEEC_FinalizeContext(&ctx);
+	printf("TEEC_FinalizeContext ok\n");
+
 	printf("Hello end\n");
 	return 0;
 }

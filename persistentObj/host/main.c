d70242613f2f79634da8e291171a0b7dff024e38
Rossi Kwak <rossi.kwak@gmail.com>
2018-01-29 16:37:29 +0900

    apply TEEC_FinalizeContext

--- persistentObj/host/main.c
+++ persistentObj/host/main.c
@@ -11,8 +11,11 @@ int main(int argc, char *argv[])
         res = TEEC_InitializeContext(NULL,&ctx);
         if(res!=TEEC_SUCCESS)
                 errx(1,"TEEC_InitializeContext failed with code 0x%x",res);
-
         printf("TEEC_InitializeContext ok\n");
+
+        printf("TEEC_FinalizeContext...\n");
+        TEEC_FinalizeContext(&ctx);
+        printf("TEEC_FinalizeContext ok\n");
 
 	printf("PersitentObj end\n");
 	return 0;

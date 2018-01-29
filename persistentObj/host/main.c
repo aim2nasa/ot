8d7d8c54f4886dbf8300ef1f7499d961a1ab9aa7
Rossi Kwak <rossi.kwak@gmail.com>
2018-01-29 16:29:21 +0900

    apply TEEC_InitializeContext

--- persistentObj/host/main.c
+++ persistentObj/host/main.c
@@ -1,7 +1,19 @@
+#include <err.h>
 #include <stdio.h>
+#include <tee_client_api.h>
 
 int main(int argc, char *argv[])
 {
+	TEEC_Result res;
+	TEEC_Context ctx;
+
+        printf("TEEC_InitializeContext...\n");
+        res = TEEC_InitializeContext(NULL,&ctx);
+        if(res!=TEEC_SUCCESS)
+                errx(1,"TEEC_InitializeContext failed with code 0x%x",res);
+
+        printf("TEEC_InitializeContext ok\n");
+
 	printf("PersitentObj end\n");
 	return 0;
 }

fe3f472f900a1f8888008e87279b8fd7d7795b70
Rossi Kwak <rossi.kwak@gmail.com>
2018-01-30 16:45:49 +0900

    implement ta_storage_cmd_create and make a call to it when receives create command

--- persistentObj/ta/persistentObj_ta.c
+++ persistentObj/ta/persistentObj_ta.c
@@ -1,6 +1,14 @@
 #include <tee_internal_api.h>
 #include <persistentObj_ta.h>
 
+#define ASSERT_PARAM_TYPE(pt) \
+do { \
+        if ((pt) != param_types) \
+                return TEE_ERROR_BAD_PARAMETERS; \
+} while (0)
+
+TEE_Result ta_storage_cmd_create(uint32_t param_types, TEE_Param params[4]);
+
 TEE_Result TA_CreateEntryPoint(void)
 {
 	DMSG("has been called");
@@ -41,9 +49,31 @@ TEE_Result TA_InvokeCommandEntryPoint(void __maybe_unused *sess_ctx,
 
 	switch(cmd_id) {
 	case TA_PERSISTENTOBJ_CMD_CREATE:
-		IMSG("PersistentObj Create command");
-		return TEE_SUCCESS;
+		IMSG("PersistentObj Create command received");
+                return ta_storage_cmd_create(param_types,params);
+
 	default:
 		return TEE_ERROR_BAD_PARAMETERS;
 	}
+}
+
+TEE_Result ta_storage_cmd_create(uint32_t param_types, TEE_Param params[4])
+{
+        TEE_Result res;
+        TEE_ObjectHandle o;
+
+        DMSG("has been called");
+        ASSERT_PARAM_TYPE(TEE_PARAM_TYPES
+                          (TEE_PARAM_TYPE_MEMREF_INPUT,
+                           TEE_PARAM_TYPE_VALUE_INOUT,
+                           TEE_PARAM_TYPE_VALUE_INPUT,
+                           TEE_PARAM_TYPE_MEMREF_INPUT));
+
+        res = TEE_CreatePersistentObject(params[2].value.b,
+                 params[0].memref.buffer, params[0].memref.size,
+                 params[1].value.a,
+                 (TEE_ObjectHandle)(uintptr_t)params[2].value.a,
+                 params[3].memref.buffer, params[3].memref.size, &o);
+        params[1].value.b = (uintptr_t)o;
+        return res;
 }

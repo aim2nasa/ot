3fa161cbc4e631f1f29fed6016732540d575cecf
Rossi Kwak <rossi.kwak@gmail.com>
2018-01-30 15:40:13 +0900

    implement fs_create function

--- persistentObj/host/main.c
+++ persistentObj/host/main.c
@@ -8,7 +8,29 @@ static TEEC_Result fs_create(TEEC_Session *sess,void *id,uint32_t id_size,
 			    uint32_t data_size, uint32_t *obj,
 			    uint32_t storage_id)
 {
-	return TEEC_ERROR_GENERIC;
+        TEEC_Operation op = {0};
+        TEEC_Result res;
+        uint32_t org;
+
+        op.params[0].tmpref.buffer = id;
+        op.params[0].tmpref.size = id_size;
+        op.params[1].value.a = flags;
+        op.params[1].value.b = 0;
+        op.params[2].value.a = attr;
+        op.params[2].value.b = storage_id;
+        op.params[3].tmpref.buffer = data;
+        op.params[3].tmpref.size = data_size;
+
+        op.paramTypes = TEEC_PARAM_TYPES(TEEC_MEMREF_TEMP_INPUT,
+                                         TEEC_VALUE_INOUT, TEEC_VALUE_INPUT,
+                                         TEEC_MEMREF_TEMP_INPUT);
+
+        res = TEEC_InvokeCommand(sess, TA_PERSISTENTOBJ_CMD_CREATE, &op, &org);
+
+        if (res == TEEC_SUCCESS)
+                *obj = op.params[1].value.b;
+
+        return res;
 }
 
 int main(int argc, char *argv[])

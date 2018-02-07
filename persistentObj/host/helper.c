47382a151adfd9200ccfd7bf19402582c7e9ff44
Rossi Kwak <rossi.kwak@gmail.com>
2018-02-07 16:25:34 +0900

    add fs_open function

--- persistentObj/host/helper.c
+++ persistentObj/host/helper.c
@@ -42,3 +42,28 @@ TEEC_Result fs_unlink(TEEC_Session *sess, uint32_t obj)
                                          TEEC_NONE, TEEC_NONE);
 	return TEEC_InvokeCommand(sess,TA_PERSISTENTOBJ_CMD_UNLINK, &op, &org);
 }
+
+TEEC_Result fs_open(TEEC_Session *sess, void *id, uint32_t id_size,
+	 	    uint32_t flags, uint32_t *obj, uint32_t storage_id)
+{
+	TEEC_Operation op = {0};
+	TEEC_Result res;
+	uint32_t org;
+
+	op.params[0].tmpref.buffer = id;
+	op.params[0].tmpref.size = id_size;
+	op.params[1].value.a = flags;
+	op.params[1].value.b = 0;
+	op.params[2].value.a = storage_id;
+
+	op.paramTypes = TEEC_PARAM_TYPES(TEEC_MEMREF_TEMP_INPUT,
+					 TEEC_VALUE_INOUT, TEEC_VALUE_INPUT,
+					 TEEC_NONE);
+
+	res = TEEC_InvokeCommand(sess, TA_PERSISTENTOBJ_CMD_OPEN, &op, &org);
+
+	if (res == TEEC_SUCCESS)
+		*obj = op.params[1].value.b;
+
+	return res;
+}

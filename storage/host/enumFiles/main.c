cd12b5830a5500cb4d3e7f67512f26ebb72a4096
Rossi Kwak <rossi.kwak@gmail.com>
2018-02-13 18:09:06 +0900

    create primitive enumFiles application

---
+++ storage/host/enumFiles/main.c
@@ -0,0 +1,55 @@
+#include <err.h>
+#include <stdio.h>
+#include <tee_api_defines.h>
+#include <ta_storage.h>
+#include "helper.h"
+
+#define MAX_FILES 3
+
+static uint8_t file_00[] = { 0x00,0x00 };
+static uint8_t file_01[] = { 0x01,0x00 };
+static uint8_t file_02[] = { 0x02,0x00 };
+static uint8_t* files[]={ file_00,file_01,file_02 };
+static uint8_t data[] = { 0x0a,0x0b,0x0c,0x0d,0x0e,0x0f,0x1a,0x1b };
+
+int main(int argc, char *argv[])
+{
+	TEEC_Result res;
+	TEEC_Context ctx;
+	TEEC_Session sess;
+	TEEC_UUID uuid = TA_STORAGE_UUID;
+	uint32_t err_origin;
+	uint32_t obj[MAX_FILES];
+	uint32_t storage_id = TEE_STORAGE_PRIVATE;
+	int i;
+
+        printf("TEEC_InitializeContext...\n");
+        res = TEEC_InitializeContext(NULL,&ctx);
+        if(res!=TEEC_SUCCESS)
+                errx(1,"TEEC_InitializeContext failed with code 0x%x",res);
+        printf("TEEC_InitializeContext ok\n");
+
+        printf("TEEC_OpenSession...\n");
+        res = TEEC_OpenSession(&ctx,&sess,&uuid,TEEC_LOGIN_PUBLIC,NULL,NULL,&err_origin);
+        if(res!=TEEC_SUCCESS)
+                errx(1,"TEEC_OpenSession failed with code 0x%x origin 0x%x",res,err_origin);
+        printf("TEEC_OpenSession ok\n");
+
+        printf("Creating in TA...\n");
+	for(i=0;i<MAX_FILES;i++) {
+		res = fs_create(&sess,files[i],sizeof(file_00),
+			TEE_DATA_FLAG_ACCESS_WRITE,
+			0,data,sizeof(data),&obj[i],storage_id);
+        	if(res!=TEEC_SUCCESS)
+                	errx(1,"fs_create failed with code 0x%x",res);
+
+        	printf("Created in TA, obj[%d]=0x%x\n",i,obj[i]);
+	}
+
+        printf("TEEC_FinalizeContext...\n");
+        TEEC_FinalizeContext(&ctx);
+        printf("TEEC_FinalizeContext ok\n");
+
+	printf("PersitentObj end\n");
+	return 0;
+}

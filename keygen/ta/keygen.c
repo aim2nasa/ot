fdb23239a8616860d20a2492fc3d646929e01da4
Rossi Kwak <rossi.kwak@gmail.com>
2018-02-22 16:28:33 +0900

    verify input parameter type to match with HA

--- keygen/ta/keygen.c
+++ keygen/ta/keygen.c
@@ -1,5 +1,11 @@
 #include "keygen.h"
 
+#define ASSERT_PARAM_TYPE(pt) \
+do { \
+	if ((pt) != param_types) \
+		return TEE_ERROR_BAD_PARAMETERS; \
+} while (0)
+
 TEE_Result ta_keygen_cmd(uint32_t param_types, TEE_Param params[4])
 {
 	TEE_Result result = TEE_SUCCESS;
@@ -7,8 +13,10 @@ TEE_Result ta_keygen_cmd(uint32_t param_types, TEE_Param params[4])
 	size_t key_size = 256;
 	TEE_ObjectInfo keyInfo;
 
-	(void)param_types;
 	(void)params;
+	ASSERT_PARAM_TYPE(TEE_PARAM_TYPES
+			  (TEE_PARAM_TYPE_VALUE_INPUT,TEE_PARAM_TYPE_MEMREF_INPUT,
+			  TEE_PARAM_TYPE_NONE,TEE_PARAM_TYPE_NONE));
 
 	if((result=TEE_AllocateTransientObject(TEE_TYPE_AES,key_size,&transient_key))!=TEE_SUCCESS){
 		EMSG("Failed to Allocate transient object handle : 0x%x",result);

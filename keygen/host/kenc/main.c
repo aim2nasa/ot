43c4870c1d5ab20116ec15bf84e35cfee6b95acd
Rossi Kwak <rossi.kwak@gmail.com>
2018-03-02 12:26:09 +0900

    add missing codes on error, resource free codes inserted

--- keygen/host/kenc/main.c
+++ keygen/host/kenc/main.c
@@ -42,16 +42,21 @@ int main(int argc, char *argv[])
 		memcpy(inp_filename,argv[2],strlen(argv[2]));
 		memcpy(out_filename,argv[3],strlen(argv[3]));
 	}else{
-		errx(1,"usage: kenc <keyfile> <inpfile> <outfile>");
+		printf("usage: kenc <keyfile> <inpfile> <outfile>\n");
+		goto cleanup1;
 	}
 
 	res = TEEC_InitializeContext(NULL,&ctx);
-	if(res!=TEEC_SUCCESS)
-		errx(1,"TEEC_InitializeContext failed with code 0x%x",res);
+	if(res!=TEEC_SUCCESS){
+		printf("TEEC_InitializeContext failed with code 0x%x\n",res);
+		goto cleanup1;
+	}
 
 	res = TEEC_OpenSession(&ctx,&sess,&uuid,TEEC_LOGIN_PUBLIC,NULL,NULL,&err_origin);
-	if(res!=TEEC_SUCCESS)
-		errx(1,"TEEC_OpenSession failed with code 0x%x origin 0x%x",res,err_origin);
+	if(res!=TEEC_SUCCESS){
+		printf("TEEC_OpenSession failed with code 0x%x origin 0x%x\n",res,err_origin);
+		goto cleanup2;
+	}
 
 	op.params[0].value.a = TEE_STORAGE_PRIVATE;
 	op.params[1].tmpref.buffer = key_filename;
@@ -59,8 +64,10 @@ int main(int argc, char *argv[])
 	op.paramTypes = TEEC_PARAM_TYPES(TEEC_VALUE_INPUT,TEEC_MEMREF_TEMP_INPUT,TEEC_VALUE_OUTPUT,TEEC_NONE);
 
 	res = TEEC_InvokeCommand(&sess,TA_KEY_OPEN_CMD,&op,&err_origin);
-	if(res!=TEEC_SUCCESS)
-		errx(1,"TEEC_InvokeCommand failed with code 0x%x origin 0x%x",res,err_origin);
+	if(res!=TEEC_SUCCESS){
+		printf("TA_KEY_OPEN_CMD TEEC_InvokeCommand failed with code 0x%x origin 0x%x\n",res,err_origin);
+		goto cleanup3;
+	}
 	
 	keyObj = op.params[2].value.a;	
 
@@ -72,8 +79,10 @@ int main(int argc, char *argv[])
 	op.params[3].value.a = keySize;
 	op.paramTypes = TEEC_PARAM_TYPES(TEEC_VALUE_OUTPUT,TEEC_VALUE_INPUT,TEEC_VALUE_INPUT,TEEC_VALUE_INPUT);
 	res = TEEC_InvokeCommand(&sess,TA_KEY_ALLOC_OPER_CMD,&op,&err_origin);
-	if(res!=TEEC_SUCCESS)
-		errx(1,"TEEC_InvokeCommand failed with code 0x%x origin 0x%x",res,err_origin);
+	if(res!=TEEC_SUCCESS){
+		printf("TA_KEY_ALLOC_OPER_CMD TEEC_InvokeCommand failed with code 0x%x origin 0x%x\n",res,err_origin);
+		goto cleanup3;
+	}
 	encOp = VAL2HANDLE(op.params[1].value.a);
 	printf("allocateOperation handle:%p\n",encOp);
 
@@ -90,10 +99,16 @@ int main(int argc, char *argv[])
 	op.params[0].value.a = keyObj;
 	op.paramTypes = TEEC_PARAM_TYPES(TEEC_VALUE_INPUT,TEEC_NONE,TEEC_NONE,TEEC_NONE);
 	res = TEEC_InvokeCommand(&sess,TA_KEY_CLOSE_CMD,&op,&err_origin);
-	if(res!=TEEC_SUCCESS)
-		errx(1,"TEEC_InvokeCommand failed with code 0x%x origin 0x%x",res,err_origin);
+	if(res!=TEEC_SUCCESS){
+		printf("TA_KEY_CLOSE_CMD TEEC_InvokeCommand failed with code 0x%x origin 0x%x\n",res,err_origin);
+		goto cleanup3;
+	}
 
+cleanup3:
+	TEEC_CloseSession(&sess);
+cleanup2:
 	TEEC_FinalizeContext(&ctx);
+cleanup1:
 	print("KeyEnc end\n");
 	return 0;
 }

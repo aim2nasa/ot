dcfa2ab3989142f11b9af0a1bd76d2813027e7d9
Rossi Kwak <rossi.kwak@gmail.com>
2018-03-06 13:27:21 +0900

    bug fix, param types and eliminate unnecessary intermediate varaible and add DMSG log

--- keygen/ta/cipher.c
+++ keygen/ta/cipher.c
@@ -16,31 +16,35 @@ TEE_Result ta_cipher_init_cmd(uint32_t param_types, TEE_Param params[4])
 TEE_Result ta_cipher_update_cmd(uint32_t param_types, TEE_Param params[4])
 {
 	TEE_Result res;
-	uint32_t outsz;
 
 	ASSERT_PARAM_TYPE(TEE_PARAM_TYPES
-			  (TEE_PARAM_TYPE_VALUE_INPUT, TEE_PARAM_TYPE_MEMREF_INPUT,
+			  (TEE_PARAM_TYPE_VALUE_INPUT, TEE_PARAM_TYPE_MEMREF_INOUT,
 			   TEE_PARAM_TYPE_MEMREF_INOUT, TEE_PARAM_TYPE_NONE));
+
+	DMSG("Cipher Update input:%s input buffer size:%d output buffer size:%d",
+		(char*)params[1].memref.buffer,params[1].memref.size,params[2].memref.size);
 
 	res = TEE_CipherUpdate(VAL2HANDLE(params[0].value.a),
 			       params[1].memref.buffer,params[1].memref.size,
-			       params[2].memref.buffer,&outsz);
-	params[2].memref.size = outsz;
+			       params[2].memref.buffer,&params[2].memref.size);
+	DMSG("Updated output size:%d",params[2].memref.size);
 	return res;
 }
 
 TEE_Result ta_cipher_do_final_cmd(uint32_t param_types, TEE_Param params[4])
 {
 	TEE_Result res;
-	uint32_t outsz;
 
 	ASSERT_PARAM_TYPE(TEE_PARAM_TYPES
-			  (TEE_PARAM_TYPE_VALUE_INPUT, TEE_PARAM_TYPE_MEMREF_INPUT,
+			  (TEE_PARAM_TYPE_VALUE_INPUT, TEE_PARAM_TYPE_MEMREF_INOUT,
 			   TEE_PARAM_TYPE_MEMREF_INOUT, TEE_PARAM_TYPE_NONE));
 
+	DMSG("Cipher DoFinal input:%s input buffer size:%d output buffer size:%d",
+		(char*)params[1].memref.buffer,params[1].memref.size,params[2].memref.size);
+
 	res = TEE_CipherDoFinal(VAL2HANDLE(params[0].value.a),
-				params[1].memref.buffer,params[1].memref.size,
-				params[2].memref.buffer,&outsz);
-	params[2].memref.size = outsz;
+			       params[1].memref.buffer,params[1].memref.size,
+			       params[2].memref.buffer,&params[2].memref.size);
+	DMSG("Updated output size:%d",params[2].memref.size);
 	return res;
 }

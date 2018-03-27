d19e562ee703f0e0d987cad70adc44a7c009c972
Rossi Kwak <rossi.kwak@gmail.com>
2018-03-27 12:33:49 +0900

    show usage when argument is not enough

--- keygen/host/keyGen/main.c
+++ keygen/host/keyGen/main.c
@@ -31,8 +31,10 @@ int main(int argc, char *argv[])
 			errx(1,"key filename is over the buffer limit(%zd)\n",sizeof(key_filename));
 
 		memcpy(key_filename,argv[1],strlen(argv[1]));
-	}else
-		memcpy(key_filename,"test.key",strlen("test.key"));
+	}else{
+                printf("usage: keygen <key filename in TEE>\n");
+                return 1;
+	}
 
 	print("key filename:%s\n",key_filename);
 

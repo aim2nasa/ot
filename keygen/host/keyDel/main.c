e2372143c4204960c4a1e3b297506ab994336413
Rossi Kwak <rossi.kwak@gmail.com>
2018-03-27 12:51:17 +0900

    delete commented lines

--- keygen/host/keyDel/main.c
+++ keygen/host/keyDel/main.c
@@ -66,15 +66,6 @@ int main(int argc, char *argv[])
 
 	printf("file open successful:%s,handle:%u\n",key_filename,keyObj);
 	
-/*
-	op.params[0].value.a = keyObj;
-	op.paramTypes = TEEC_PARAM_TYPES(TEEC_VALUE_INPUT,TEEC_NONE,TEEC_NONE,TEEC_NONE);
-	res = TEEC_InvokeCommand(&sess,TA_KEY_CLOSE_CMD,&op,&err_origin);
-	if(res!=TEEC_SUCCESS)
-		errx(1,"TEEC_InvokeCommand failed with code 0x%x origin 0x%x",res,err_origin);
-
-	printf("file close successful:%s,handle:%u\n",key_filename,keyObj);
-*/
 	op.params[0].value.a = keyObj;
 	op.paramTypes = TEEC_PARAM_TYPES(TEEC_VALUE_INPUT,TEEC_NONE,TEEC_NONE,TEEC_NONE);
 	res = TEEC_InvokeCommand(&sess,TA_KEY_UNLINK_CMD,&op,&err_origin);

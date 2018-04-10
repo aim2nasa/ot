a5d61a48e0acc20917f4e435ab68ed451ada1065
Rossi Kwak <rossi.kwak@gmail.com>
2018-04-10 17:35:56 +0900

    add openSession,closeSession methods

--- keygen/host/libokey/okey.c
+++ keygen/host/libokey/okey.c
@@ -12,3 +12,17 @@ void finalizeContext(okey *o)
 	TEEC_FinalizeContext(o->ctx);
 	free(o->ctx);
 }
+
+TEEC_Result openSession(okey *o,const TEEC_UUID *destination,uint32_t connectionMethod,
+			const void *connectionData,TEEC_Operation *operation)
+{
+	o->session = (TEEC_Session*)malloc(sizeof(TEEC_Session));
+	return TEEC_OpenSession(o->ctx,o->session,destination,connectionMethod,
+		connectionData,operation,&o->returnOrigin);
+}
+
+void closeSession(okey *o)
+{
+	TEEC_CloseSession(o->session);
+	free(o->session);
+}
--- keygen/host/libokey/okey.h
+++ keygen/host/libokey/okey.h
@@ -9,9 +9,14 @@ extern "C" {
 
 	typedef struct _optee_key_context {
 		TEEC_Context *ctx;
+		TEEC_Session *session;
+		uint32_t returnOrigin;
 	} okey;
 
 	TEEC_Result initializeContext(const char *name,okey *o);
+	TEEC_Result openSession(okey *o,const TEEC_UUID *destination,uint32_t connectionMethod,
+				const void *connectionData,TEEC_Operation *operation);
+	void closeSession(okey *o);
 	void finalizeContext(okey *o);
 
 #ifdef __cplusplus

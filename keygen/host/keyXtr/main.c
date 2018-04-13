5ee012a462d84e3ea110cd3e1f3d414a413e5fa8
Rossi Kwak <rossi.kwak@gmail.com>
2018-04-13 13:03:09 +0900

    replace implementation using libokey, add dependency on tee_api_defines.h explicitly

--- keygen/host/keyXtr/main.c
+++ keygen/host/keyXtr/main.c
@@ -1,13 +1,9 @@
 #include <err.h>
 #include <stdio.h>
-#include <tee_api_types.h>
-#include <tee_client_api.h>
-#include <keygen_ta.h>
 #include <string.h>
 #include <assert.h>
-
-#define TEEC_OPERATION_INITIALIZER	{ 0 }
-#define TEE_STORAGE_PRIVATE		0x00000001
+#include <okey.h>
+#include <tee_api_defines.h>
 
 int print(const char *format,...)
 {
@@ -18,45 +14,12 @@ int print(const char *format,...)
 #endif
 }
 
-TEEC_Result get_object_buffer_attribute(TEEC_Session *s,
-                                                     TEE_ObjectHandle o,
-                                                     uint32_t attr_id,
-                                                     void *buf, size_t *blen)
-{
-        TEEC_Result res;
-        TEEC_Operation op = TEEC_OPERATION_INITIALIZER;
-        uint32_t ret_orig;
-
-        assert((uintptr_t)o <= UINT32_MAX);
-        op.params[0].value.a = (uint32_t)(uintptr_t)o;
-        op.params[0].value.b = attr_id;
-
-        op.params[1].tmpref.buffer = buf;
-        op.params[1].tmpref.size = *blen;
-
-        op.paramTypes = TEEC_PARAM_TYPES(TEEC_VALUE_INPUT,
-                                         TEEC_MEMREF_TEMP_OUTPUT, TEEC_NONE,
-                                         TEEC_NONE);
-
-        res = TEEC_InvokeCommand(s, TA_KEY_GET_OBJECT_BUFFER_ATTRIBUTE_CMD,
-                                 &op, &ret_orig);
-
-        if (res == TEEC_SUCCESS)
-                *blen = op.params[1].tmpref.size;
-
-        return res;
-}
-
 int main(int argc, char *argv[])
 {
 	TEEC_Result res;
-	TEEC_Context ctx;
-	TEEC_Session sess;
-	TEEC_UUID uuid = TA_KEYGEN_UUID;
-	uint32_t err_origin;
-	TEEC_Operation op = TEEC_OPERATION_INITIALIZER;
 	uint8_t key_filename[256]={ 0 },key_buffer[1024]={ 0 };
-	TEE_ObjectHandle keyObj = (TEE_ObjectHandle)NULL;
+	okey o;
+	uint32_t keyObj=0;
 	size_t i,key_size;
 	FILE *fp;
 
@@ -74,38 +37,29 @@ int main(int argc, char *argv[])
 	printf("key filename:%s\n",key_filename);
 	if(argc>2) printf("key dump file:%s\n",argv[2]);
 
-	print("TEEC_InitializeContext...\n");
-	res = TEEC_InitializeContext(NULL,&ctx);
+	print("initializeContext...\n");
+	res = initializeContext(NULL,&o);
 	if(res!=TEEC_SUCCESS)
-		errx(1,"TEEC_InitializeContext failed with code 0x%x",res);
-	print("TEEC_InitializeContext ok\n");
+		errx(1,"initializeContext failed with code 0x%x",res);
+	print("initializeContext ok\n");
 
-	print("TEEC_OpenSession...\n");
-	res = TEEC_OpenSession(&ctx,&sess,&uuid,TEEC_LOGIN_PUBLIC,NULL,NULL,&err_origin);
+	print("openSession...\n");
+	res = openSession(&o,TEEC_LOGIN_PUBLIC,NULL,NULL);
 	if(res!=TEEC_SUCCESS)
-		errx(1,"TEEC_OpenSession failed with code 0x%x origin 0x%x",res,err_origin);
-	print("TEEC_OpenSession ok\n");
+		errx(1,"openSession failed with code 0x%x origin 0x%x",res,o.error);
+	print("openSession ok\n");
 
-	print("Invoking TA...\n");
-	op.params[0].value.a = TEE_STORAGE_PRIVATE;
-	op.params[1].tmpref.buffer = key_filename;
-	op.params[1].tmpref.size = strlen((const char*)key_filename);
-	op.paramTypes = TEEC_PARAM_TYPES(TEEC_VALUE_INPUT,TEEC_MEMREF_TEMP_INPUT,TEEC_VALUE_OUTPUT,TEEC_NONE);
-
-	res = TEEC_InvokeCommand(&sess,TA_KEY_OPEN_CMD,&op,&err_origin);
+	res = keyOpen(&o,TEE_STORAGE_PRIVATE,(char*)key_filename,&keyObj);
 	if(res!=TEEC_SUCCESS)
-		errx(1,"TEEC_InvokeCommand failed with code 0x%x origin 0x%x",res,err_origin);
+		errx(1,"keyOpen failed with code 0x%x origin 0x%x",res,o.error);
 	
-	keyObj = (TEE_ObjectHandle)(uintptr_t)op.params[2].value.a;
-	print("TA Invoked\n");
-
-	printf("file open successful:%s,handle:%p\n",key_filename,keyObj);
+	printf("file open successful:%s,handle:%u\n",key_filename,keyObj);
 
 	key_size = sizeof(key_buffer);
-	res = get_object_buffer_attribute(&sess,keyObj,TEE_ATTR_SECRET_VALUE,key_buffer,&key_size);
-	if(res!=TEEC_SUCCESS){
-		errx(1,"get_object_buffer_attribute failed with code 0x%x",res);
-	}
+	res = keyGetObjectBufferAttribute(&o,keyObj,TEE_ATTR_SECRET_VALUE,key_buffer,&key_size);
+	if(res!=TEEC_SUCCESS)
+		errx(1,"keyGetObjectBufferAttribute failed with code 0x%x",res);
+
 	if(argc>2){
 		if((fp=fopen(argv[2],"wb"))!=NULL){
 			int nWrite = fwrite(key_buffer,1,key_size,fp);
@@ -119,18 +73,16 @@ int main(int argc, char *argv[])
 	for(i=0;i<key_size;i++) printf("%x ",key_buffer[i]);
 	printf("\n");
 	
-	op.params[0].value.a = (uint32_t)(uintptr_t)keyObj;
-	op.paramTypes = TEEC_PARAM_TYPES(TEEC_VALUE_INPUT,TEEC_NONE,TEEC_NONE,TEEC_NONE);
-	res = TEEC_InvokeCommand(&sess,TA_KEY_CLOSE_CMD,&op,&err_origin);
+	res = keyClose(&o,keyObj);
 	if(res!=TEEC_SUCCESS)
-		errx(1,"TEEC_InvokeCommand failed with code 0x%x origin 0x%x",res,err_origin);
+		errx(1,"keyClose failed with code 0x%x",res);
 
-	printf("file close successful:%s,handle:%p\n",key_filename,keyObj);
+	printf("file close successful:%s,handle:%u\n",key_filename,keyObj);
 
-	print("TEEC_FinalizeContext...\n");
-	TEEC_FinalizeContext(&ctx);
-	print("TEEC_FinalizeContext ok\n");
+	print("finalizeContext...\n");
+	finalizeContext(&o);
+	print("finalizeContext ok\n");
 
-	print("KeyOpen end\n");
+	print("KeyXtr end\n");
 	return 0;
 }

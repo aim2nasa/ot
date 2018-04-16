d161cc66ee1fcbf7db0867df1e8e23791f55f564
Rossi Kwak <rossi.kwak@gmail.com>
2018-04-16 18:40:28 +0900

    delete enumHelper.* and just depend on libokey only. modify main.c to work with libokey

--- keygen/host/keyEnum/main.c
+++ keygen/host/keyEnum/main.c
@@ -1,13 +1,6 @@
 #include <err.h>
 #include <stdio.h>
-#include <tee_client_api.h>
-#include <keygen_ta.h>
-#include <string.h>
-#include "enumHelper.h"
-#include <stdlib.h>
 #include <okey.h>
-
-#define TEE_STORAGE_PRIVATE		0x00000001
 
 int print(const char *format,...)
 {
@@ -21,15 +14,8 @@ int print(const char *format,...)
 int main(int argc, char *argv[])
 {
 	TEEC_Result res;
-	uint32_t enumHandle;
-	uint32_t storage_id = TEE_STORAGE_PRIVATE;
-	size_t infoSize;
-	uint32_t idSize;
-	int i,j;
-	uint8_t info[256];
-	uint8_t id[256+1];	//+1 to include null at the end of name
-	char *fileName = 0;
 	okey o;
+	eObjList *list = NULL;
 
 	print("initializeContext...\n");
 	res = initializeContext(NULL,&o);
@@ -43,43 +29,11 @@ int main(int argc, char *argv[])
 		errx(1,"openSession failed with code 0x%x origin 0x%x",res,o.error);
 	print("openSession ok\n");
 
-	res = fs_alloc_enum(o.session,&enumHandle);
+	res = keyEnumObjectList(&o,TEE_STORAGE_PRIVATE,&list);
         if(res!=TEEC_SUCCESS)
-                errx(1,"fs_alloc_enum failed with code 0x%x",res);
-	print("enum handle 0x%x\n",enumHandle);
+                errx(1,"keyEnumObjectList failed with code 0x%x",res);
 
-	res = fs_start_enum(o.session,enumHandle,storage_id);
-        if(res!=TEEC_SUCCESS)
-                errx(1,"fs_start_enum failed with code 0x%x",res);
-
-	i=0;
-	memset((void*)info,0,sizeof(info));
-	memset((void*)id,0,sizeof(id));
-	infoSize = sizeof(info);
-	idSize = sizeof(id);
-
-	printf("tool to list up(enumerate) keys stored in trustzone\n");
-	while(TEEC_SUCCESS==fs_next_enum(o.session,enumHandle,info,&infoSize,id,&idSize)){
-		printf("[%d] infoSize:%zd,idSize:%u \n",i++,infoSize,idSize);
-		printf(" id:"); 
-		fileName = malloc(idSize);
-		memcpy(fileName,id,idSize);
-		fileName[idSize]=0;
-		printf("%s",fileName); 
-		printf("\n info:"); 
-		for(j=0;j<infoSize;j++) {
-			printf("%x",info[j]); 
-		}
-		printf("\n"); 
-
-		infoSize = sizeof(info);
-		idSize = sizeof(id);
-		free(fileName);
-	}
-
-	res = fs_free_enum(o.session,enumHandle);
-        if(res!=TEEC_SUCCESS)
-                errx(1,"fs_free_enum failed with code 0x%x",res);
+	printf("list=%p\n",list);
 
 	print("finalizeContext...\n");
 	finalizeContext(&o);

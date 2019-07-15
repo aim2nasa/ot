c9c6360df159d01c4a66450b980afbf6490fd960
Rossi Kwak <rossi.kwak@gmail.com>
2019-07-15 16:55:38 +0900

    reference implementation of file copy to NFS

---
+++ tnfs2.6.sh
@@ -0,0 +1,19 @@
+cp hello/host/hello /srv/nfs/linaro-stretch-developer-20171109-88/root
+cp hello/ta/3c45d356-f9d5-11e7-8c3f-9a214cf093ae.ta /srv/nfs/linaro-stretch-developer-20171109-88/lib/optee_armtz
+
+cp keygen/host/keyDel/keydel /srv/nfs/linaro-stretch-developer-20171109-88/root
+cp keygen/host/keyEncDec/ked /srv/nfs/linaro-stretch-developer-20171109-88/root
+cp keygen/host/keyEnum/keyenum /srv/nfs/linaro-stretch-developer-20171109-88/root
+cp keygen/host/keyGen/keygen /srv/nfs/linaro-stretch-developer-20171109-88/root
+cp keygen/host/keyInj/keyinj /srv/nfs/linaro-stretch-developer-20171109-88/root
+cp keygen/host/keyOpen/keyopen /srv/nfs/linaro-stretch-developer-20171109-88/root
+cp keygen/host/keyOpen/keyopen /srv/nfs/linaro-stretch-developer-20171109-88/root
+cp keygen/host/keyXtr/keyxtr /srv/nfs/linaro-stretch-developer-20171109-88/root
+cp keygen/host/libokey/libokey.so.1 /srv/nfs/linaro-stretch-developer-20171109-88/lib
+cp keygen/ta/ab201cb2-177d-11e8-b642-0ed5f89f718b.ta /srv/nfs/linaro-stretch-developer-20171109-88/lib/optee_armtz
+
+cp persistentObj/host/persistentObj /srv/nfs/linaro-stretch-developer-20171109-88/root
+cp persistentObj/ta/cfe62966-04cb-11e8-ba89-0ed5f89f718b.ta /srv/nfs/linaro-stretch-developer-20171109-88/lib/optee_armtz
+
+cp storage/host/perObj/perObj /srv/nfs/linaro-stretch-developer-20171109-88/root
+cp storage/ta/b689f2a7-8adf-477a-9f99-32e90c0ad0a2.ta /srv/nfs/linaro-stretch-developer-20171109-88/lib/optee_armtz

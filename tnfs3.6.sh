ee00fafcaac8683df3f4311588c141bfe191f4d9
Rossi Kwak <rossi.kwak@gmail.com>
2019-07-16 12:36:03 +0900

    add missing folder in making

--- tnfs3.6.sh
+++ tnfs3.6.sh
@@ -25,5 +25,6 @@ cp keygen/ta/ab201cb2-177d-11e8-b642-0ed5f89f718b.ta $TA_DIR
 cp persistentObj/host/persistentObj $ROOT
 cp persistentObj/ta/cfe62966-04cb-11e8-ba89-0ed5f89f718b.ta $TA_DIR
 
+cp storage/host/enumFiles/enumFiles $ROOT
 cp storage/host/perObj/perObj $ROOT
 cp storage/ta/b689f2a7-8adf-477a-9f99-32e90c0ad0a2.ta $TA_DIR
